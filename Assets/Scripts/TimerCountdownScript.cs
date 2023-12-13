c58454e8df88e2987a44ff0a75513eeffae76ded
DiegoSC-29 <serrcdi19@gmail.com>
2023-12-13 15:28:56 -0600

    Timer 3 Minutos

---
+++ Assets/Scripts/TimerCountdownScript.cs
@@ -0,0 +1,77 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerCountdownScript : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remianingTime;
+    void Update()
+    {
+        if (remianingTime > 0)
+        {
+            remianingTime -= Time.deltaTime;
+        }
+        else if(remianingTime < 0)
+        {
+            remianingTime = 0;
+            //GameOver();
+            timerText.color = Color.red;
+        }
+        int minutes = Mathf.FloorToInt(remianingTime / 60);
+        int seconds = Mathf.FloorToInt(remianingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}",minutes,seconds);
+    }
+}*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerCountdownScript : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float initialTime;
+    private float remainingTime;
+    private bool timerStarted = false;
+
+    void Start()
+    {
+        remainingTime = initialTime;
+    }
+
+    void Update()
+    {
+        if (timerStarted && remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+            UpdateTimerUI();
+        }
+        else if (remainingTime <= 0)
+        {
+            remainingTime = 0;
+            // GameOver();
+            timerText.color = Color.red;
+            UpdateTimerUI();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Change the tag to match your player's tag
+        {
+            timerStarted = true;
+        }
+    }
+
+    void UpdateTimerUI()
+    {
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
