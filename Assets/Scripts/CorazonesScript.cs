24e07985819c6d1822073fe0221eab258b65074e
thepatochido <80381371+thepatochido@users.noreply.github.com>
2023-12-13 18:27:36 -0600

    Creación del CorazonesScript
    Es un playerHealthScript pero que funciona con corazones singulares en lugar de barra de vida. Falta agregar Black Image y agregarle el damageToPlayer al doggy

--- Assets/Scripts/CorazonesScript.cs
+++ Assets/Scripts/CorazonesScript.cs
@@ -1,16 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Collections;
 
 public class CorazonesScript : MonoBehaviour
 {
     public int health;
     public int numDeCorazones;
+    private bool hasDied = false;
 
     public Image[] hearts;
     public Sprite fullHeart;
     public Sprite emptyHeart;
+    public GameObject blackImage;
 
     private void Update()
     {
@@ -24,10 +26,12 @@ public class CorazonesScript : MonoBehaviour
             if (i < health)
             {
                 hearts[i].sprite = fullHeart;
+                hearts[i].rectTransform.localScale = new Vector3(0.6f,0.6f,0.99f);
             }
             else
             {
                 hearts[i].sprite = emptyHeart;
+                hearts[i].rectTransform.localScale = new Vector3(0.5f,0.5f,1);
             }
 
             if(i<numDeCorazones)
@@ -39,5 +43,22 @@ public class CorazonesScript : MonoBehaviour
                 hearts[i].enabled=false;
             }
         }
+    }
+
+    public void DamagePlayer(int damageToPlayer)
+    {
+        health -= damageToPlayer;
+        if (health <= 0 && !hasDied)
+        {
+            hasDied = true;
+            blackImage.SetActive(true);
+            StartCoroutine(DelaySceneChange());
+        }
+    }
+
+    private IEnumerator DelaySceneChange()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("EndScene", LoadSceneMode.Single);
     }
 }
