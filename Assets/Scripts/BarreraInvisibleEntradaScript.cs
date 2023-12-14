206e708eb393c9535e8fa9e3e31a7840d9de2c9b
luisgleonv <luisgleonv@gmail.com>
2023-12-13 22:01:09 -0600

    Script de alarma
    Agregue el script de la alarma y todo lo relacionado. De su contador podemos sacar para acabar el juego

--- Assets/Scripts/BarreraInvisibleEntradaScript.cs
+++ Assets/Scripts/BarreraInvisibleEntradaScript.cs
@@ -4,43 +4,84 @@ using UnityEngine;
 
 public class BarreraInvisibleEntradaScript : MonoBehaviour
 {
-   public AudioSource alarmaAudioSource;
-    public float tiempoDeAlarma = 100f;  // Duración del temporizador en segundos
+    public AudioSource alarmaAudioSource;
+    public AudioClip sonidoAlTocar;
+    public float tiempoDeAlarma = 90f;
     private float tiempoRestante;
+    public float rangoDeApagado = 5f; // Nuevo rango para apagar la alarma
+    private bool hasItTurnedOn = false; //Es para evitar multiples iteraciones
 
     private bool alarmaActivada = false;
 
     void Start()
     {
-        tiempoRestante = tiempoDeAlarma; // Inicializar el tiempo restante
+        tiempoRestante = tiempoDeAlarma;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && !alarmaActivada)
+        if (other.CompareTag("Player") && !alarmaActivada && !hasItTurnedOn)
         {
-            // Activar la alarma
             alarmaAudioSource.Play();
-
-            // Iniciar el temporizador progresivo
-            InvokeRepeating("ActualizarTemporizador", 0f, 1f); // Llama a ActualizarTemporizador cada segundo
-
+            InvokeRepeating("ActualizarTemporizador", 0f, 1f);
             alarmaActivada = true;
+            hasItTurnedOn = true;
         }
     }
 
     private void ActualizarTemporizador()
     {
-        tiempoRestante -= 1f; // Decrementar el tiempo restante
+        tiempoRestante -= 1f;
 
         if (tiempoRestante <= 0f)
         {
-            // Si el tiempo se agota, detener la alarma y detener la invocación repetida
             alarmaAudioSource.Stop();
             CancelInvoke("ActualizarTemporizador");
+            alarmaActivada = false; // Desactiva la alarma cuando el tiempo ha llegado a cero
         }
 
-        // Aquí puedes realizar acciones adicionales con el tiempo restante, como mostrarlo en la interfaz de usuario.
         Debug.Log("Tiempo restante: " + tiempoRestante);
+    }
+
+    void Update()
+    {
+        // Verifica si se presiona la tecla "R" y el jugador está cerca de la alarma
+        if (Input.GetKeyDown(KeyCode.R) && alarmaActivada && EsJugadorCerca())
+        {
+            ApagarAlarma();
+        }
+
+    }
+
+   bool EsJugadorCerca()
+    {
+        // Verifica si el jugador está dentro del rango de apagado
+        Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        Vector3 alarmaPosition = GameObject.FindGameObjectWithTag("AlarmaObjeto").transform.position;
+        playerPosition.y = alarmaPosition.y; // Ajusta la posición Y para que sea la misma que la del objeto
+    
+
+        float distancia = Vector3.Distance(playerPosition,  alarmaPosition);
+        Debug.Log("Distancia entre jugador y alarma: " + distancia);
+
+        bool estaCerca = distancia <= rangoDeApagado;
+        Debug.Log("¿El jugador está cerca? " + estaCerca);
+
+        return estaCerca;
+    }
+
+    void ApagarAlarma()
+    {
+        alarmaAudioSource.Stop();
+        tiempoRestante += 30f;
+        alarmaActivada = false; // Desactiva la alarma después de apagarla
+
+        // Reproducir el sonido al tocar
+        if (sonidoAlTocar != null)
+        {
+            AudioSource.PlayClipAtPoint(sonidoAlTocar, transform.position);
+        }
+
+        Debug.Log("Alarma apagada. Tiempo restante: " + tiempoRestante);
     }
 }
