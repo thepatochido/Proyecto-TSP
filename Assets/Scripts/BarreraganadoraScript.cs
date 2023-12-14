addd802be49caeea117fcc48d228cb42ee44021c
luisgleonv <luisgleonv@gmail.com>
2023-12-14 03:44:53 -0600

    Arregle Escena Win
    Los botones ya funcionan correctamente

--- Assets/Scripts/BarreraganadoraScript.cs
+++ Assets/Scripts/BarreraganadoraScript.cs
@@ -1,24 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BarreraganadoraScript : MonoBehaviour
 {
     public ControladorColeccionablesScript controladorColeccionables;
+
+    public GameObject blackImage;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && controladorColeccionables.collectedCount == controladorColeccionables.totalCollectibles)
         {
             Debug.Log("¡Ganaste!");
+            blackImage.SetActive(true);
+            StartCoroutine(DelaySceneChange());
           
         }
     }
 
-    /*private IEnumerator DelaySceneChange()
+    private IEnumerator DelaySceneChange()
     {
         yield return new WaitForSeconds(2);
-        SceneManager.LoadScene("WinScene", LoadSceneMode.single);
-
-    }*/
+        SceneManager.LoadScene("WinScene", LoadSceneMode.Single);
+    }
 }
